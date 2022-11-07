ee17f428046159be46ec663a377b5abe07f3194a
LHTcode <LHT13535837097@163.com>
2022-11-07 21:45:49 +0800

    Add some globale config.

--- config/global_config.py
+++ config/global_config.py
@@ -5,6 +5,9 @@ class GlobalConfig():
     def __init__(self):
         self.__dataset_path = "/media/lihanting/Elements/visual-group/ArmorId"
         self.__device = "cuda:0" if cuda.is_available() else "cpu"
+        self.__model_name = "MobileNetv2"
+        self.__classes_num = 8
+        self.__input_size = (56, 56)
     @property
     def DATASET_PATH(self):
         assert Path(self.__dataset_path).exists(), NotADirectoryError(f"DATASET_PATH {self.__dataset_path} no found!")
@@ -12,5 +15,14 @@ class GlobalConfig():
     @property
     def DEVICE(self):
         return self.__device
+    @property
+    def MODEL_NAME(self):
+        return self.__model_name
+    @property
+    def CLASSES_NUM(self):
+        return self.__classes_num
+    @property
+    def INPUT_SIZE(self):
+        return self.__input_size
 
 global_config = GlobalConfig()
