8f4e6a50fcc0f69a3a974c00b4f88d209f83b023
LHTcode <LHT13535837097@163.com>
2022-11-07 21:51:14 +0800

    Add test function.

--- train.py
+++ train.py
@@ -4,20 +4,22 @@ from torch.utils.data import DataLoader
 from torch.optim import SGD
 from torch.utils.tensorboard import SummaryWriter
 from pathlib import Path
+import copy
 
 from model.MobileNetv2 import MobileNetv2, ont_hot_cross_entropy
+from model.LeNet import LeNet
 from config.global_config import global_config
 from data.dataset import NumberDataset
 from data.classes import NUMBER_CLASSES
-from tools.pytorch2onnx import convert_ONNX
 
 @torch.no_grad()
-def test(test_dataloader, model_save_path, writer, ep):
-    print("Testing...\n")
+def test(model, test_dataloader, model_save_path, writer, ep):
+    print(f"Testing {model.name}...\n")
     test_loop = tqdm(test_dataloader)
     correct_num = 0
-    model = MobileNetv2(wid_mul=0.5).to("cuda:0")
-    model.load_state_dict(torch.load(model_save_path, map_location=torch.device("cuda:0"))["state"])
+    model.load_state_dict(torch.load(model_save_path, map_location=torch.device(global_config.DEVICE))["state"])
+    model.eval()
+
     for img, label in test_loop:
         output = model(img)
         correct_num += torch.sum(torch.max(output, dim=1)[-1] == torch.max(label, dim=1)[-1])
@@ -27,11 +29,14 @@ def test(test_dataloader, model_save_path, writer, ep):
 
 @torch.enable_grad()
 def train(model, dataloader, test_dataloader, epoch, writer):
+    test_model = copy.deepcopy(model)
+    test_model.to(torch.device(global_config.DEVICE))
+
     model_save_path = Path.cwd() / "model_params"
     if not model_save_path.is_dir():
         Path.mkdir(model_save_path)
-    model_save_path = model_save_path / 'best_model.pt'
-    optim = SGD(model.parameters(), 0.001)
+    model_save_path = model_save_path / (global_config.MODEL_NAME + '.pt')
+    optim = SGD(model.parameters(), 0.1)
 
     epoch_loop = tqdm(range(epoch), total=epoch)
     train_count = 1  # 用于计算runtime_loss
@@ -59,22 +64,23 @@ def train(model, dataloader, test_dataloader, epoch, writer):
         writer.add_scalar('Precision', precision, ep)
         save_state_dict = {"state": model.state_dict()}
         torch.save(save_state_dict, str(model_save_path))
-        print("Successfully save model_params state.\n")
+        print(f"Successfully save {model.name} model_params state.\n")
         if (ep+1) % 1 == 0:
-            pass
-            # test(test_dataloader, model_save_path, writer, ep)
+            test(test_model, test_dataloader, model_save_path, writer, ep)
 
 if __name__ == '__main__':
     root_path = global_config.DATASET_PATH
-    model = MobileNetv2(wid_mul=0.5, output_channels=NUMBER_CLASSES).to("cuda:0")
-    dataset = NumberDataset(root_path, input_size=(28, 28), classes_num=NUMBER_CLASSES)
-    # test_dataset = NumberDataset(root_path, input_size=(52, 52), phase="test")
+    model = MobileNetv2(wid_mul=1, output_channels=NUMBER_CLASSES).to(global_config.DEVICE)
+    # model = LeNet(classes_num=NUMBER_CLASSES).to(global_config.DEVICE)
+    dataset = NumberDataset(root_path, input_size=global_config.INPUT_SIZE, classes_num=NUMBER_CLASSES)
+    test_dataset = NumberDataset(root_path, classes_num=global_config.CLASSES_NUM, input_size=global_config.INPUT_SIZE, phase="test")
 
     training_config = {
         "batch_size": 80,
-        "epoch": 20
+        "epoch": 10
     }
     dataloader = DataLoader(dataset, training_config['batch_size'], True)
-    # test_dataloader = DataLoader(test_dataset, 1, True)
+    test_dataloader = DataLoader(test_dataset, 1, True)
     writer = SummaryWriter()
-    train(model, dataloader, None, training_config['epoch'], writer)
+    # train(model, dataloader, test_dataloader, training_config['epoch'], writer)
+    test(model, test_dataloader, "/home/lihanting/PycharmProjects/DigitalRecognition/model_params/" + global_config.MODEL_NAME + '.pt', writer, 0)
