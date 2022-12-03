4316eccd20e8c4d5f7428aa6bd997ba37b357d77
LHTcode <LHT13535837097@163.com>
2022-12-03 21:41:13 +0800

    Add DataAugmentation class.

---
+++ data/data_argumentation.py
@@ -0,0 +1,112 @@
+import torch
+from torchvision import transforms as T
+from torchvision.transforms import functional as F
+from config.global_config import global_config
+from collections import namedtuple
+from typing import Tuple
+
+class RandomNoise(T.RandomErasing):
+    """Make some white point noise.
+    This class inherits from torchvision.transforms.RandomErasing. The detail
+    of parameters introduction see https://pytorch.org/vision/stable/generated/torchvision.transforms.RandomErasing.html?highlight=randomerasing#randomerasing
+    'Random Erasing Data Augmentation' by Zhong et al. See https://arxiv.org/abs/1708.04896
+
+    Args:
+        p: probability that the random noise operation will be performed.
+        scale: range of proportion of erased area against input image.
+        ratio: range of aspect ratio of erased area.
+        erasing_area: white point noise area, default is 100.
+
+    Returns:
+        Added white point noise Image.
+    """
+    def __init__(self, p=0.5, scale=(0.0006, 0.0006), ratio=(1, 1), erasing_area=100):
+        super(RandomNoise, self).__init__(1, scale, ratio, 255)
+        self.p = p
+        self.erasing_area = erasing_area
+
+    def forward(self, img):
+        if torch.rand(1) < self.p:
+            value = [self.value]
+            for _ in range(self.erasing_area):
+                if value is not None and not (len(value) in (1, img.shape[-3])):
+                    raise ValueError(
+                        "If value is a sequence, it should have either a single value or "
+                        f"{img.shape[-3]} (number of input channels)"
+                    )
+                x, y, h, w, v = self.get_params(img, scale=self.scale, ratio=self.ratio, value=value)
+                img = F.erase(img, x, y, h, w, v, self.inplace)
+            return img
+        return img
+
+class DataAugmentation:
+    """Provide some data augmentation method.
+
+    Note: You should give names of some data argumentation methods name,
+    and use get_data_argumentation_compose method to get a transform compose.
+
+    Args:
+        methods (tuple of str): except some data argumentation methods name.
+        img_shape (tuple of int): except the img shape that require data argumentation.
+    """
+    def __init__(self, methods: Tuple[str, ...], img_shape: Tuple[int, int]):
+        Shape = namedtuple(typename="img_shape", field_names=['H', 'W'])
+        self.img_shape = Shape(img_shape[0], img_shape[1])
+        self.transforms_compose = list()
+        for method in methods:
+            if method == 'random_resized_crop':
+                self.transforms_compose.append(self._get_random_resized_crop(self.img_shape))
+            elif method == 'random_noise':
+                self.transforms_compose.append(self._get_random_noise())
+            elif method == 'mix_up':
+                pass
+            elif method == 'cut_mix':
+                pass
+            else:
+                print(f"Warning: method named {method} is no found!")
+
+    def get_data_argumentation_compose(self) -> T.Compose:
+        return T.Compose(self.transforms_compose)
+
+    @staticmethod
+    def _get_random_resized_crop(img_shape: namedtuple) -> torch.nn.Module:
+        h, w = img_shape.H, img_shape.W
+        r = w / h
+        transform = T.RandomResizedCrop(
+            size=(h, w),
+            scale=(0.5, 1),
+            ratio=(r, r),
+            interpolation=T.InterpolationMode.BICUBIC,
+        )
+        return transform
+
+    @staticmethod
+    def _get_random_noise() -> torch.nn.Module:
+        transform = RandomNoise(
+            p=0.5,
+            scale=(0.0006, 0.0006),
+            ratio=(1, 1),
+            erasing_area=120
+        )
+        return transform
+
+    @staticmethod
+    def _get_mix_up():
+        pass
+
+    @staticmethod
+    def _get_cut_mix():
+        pass
+
+
+if __name__ == '__main__':
+    import cv2
+    dataset_path = global_config.DATASET_PATH
+    src_img = cv2.imread(dataset_path + "/train" + '/2' + '/252.png', cv2.IMREAD_GRAYSCALE)
+    src_img = torch.from_numpy(src_img)
+    data_arug = DataAugmentation(('random_resized_crop', 'random_noise'), src_img.shape)
+    transform = data_arug.get_data_argumentation_compose()
+    for i in range(10):
+        random_resized_crop_img = transform(src_img.unsqueeze(0))
+        cv2.imshow("random_resized_crop_img", random_resized_crop_img.squeeze().numpy())
+        cv2.waitKey(0)
